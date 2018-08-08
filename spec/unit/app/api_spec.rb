ad47c3af1f117e424512e0f382e8ab93d8cc3152
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-08-08 16:56:17 +1000

    sketch the tests

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../../app/api'
+
+module ExpenseTracker 
+    RSpec.describe API do
+        describe 'POST /expenses' do
+            context 'when the expense is successfully recorded' do
+                it 'returns the expense id'
+                it 'responds with a 200 (OK)' 
+            end
+                # ... next context will go here...
+        end 
+    end
+end
